01aafd67939aba528f69d9abfec3710d19ead9a6
author-sanjay <sanjaykumar73189@gmail.com>
2020-06-29 14:53:09 +0530

    main class for chopsticks game

---
+++ mainclass.java
@@ -0,0 +1,21 @@
+// main class for chopsticks game
+//it is still not complete so please don't judge
+
+class Main{
+    public static void main(String[] arg){
+        Scanner scanner=new scanner(system.in);
+        System.out.print("Enter first name");
+        String first_name=scanner.next();
+        System.out.println("Enter last name");
+        public int user_lefthand=1;
+        public int user_righthand=1;
+        public int comp_lefthand=1;
+        public int comp_righthand=1;
+        public int counter=1;
+
+        Match match=new Match();
+        match.printData(user_lefthand, user_righthand, comp_lefthand, comp_righthand)
+        
+
+    }
+}

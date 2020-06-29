b0d75f348340921d47eea15d70e27be0da41c7d1
author-sanjay <sanjaykumar73189@gmail.com>
2020-06-29 14:55:37 +0530

    added printing commands

---
+++ printData.java
@@ -0,0 +1,11 @@
+
+//this is to print score
+
+class Match{
+    public static void printData(int user_lefthand,int user_righthand,int comp_lefthand=0,int comp_righthand){
+        System.out.println("Your left hand score"+user_lefthand);
+        System.out.println("Your right hand score"+user_righthand);
+        System.out.println("Player 2's left hand score"+comp_lefthand);
+        System.out.println("Player 2's right hand score"+comp_righthand);
+    }
+}
